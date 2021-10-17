5bc733e266d0d50aab046501c09ac6399b6c0077
NamoSmith <namosmith2545@gmail.com>
2021-10-18 00:38:58 +0700

    Refactoring.

--- movie.py
+++ movie.py
@@ -1,23 +1,44 @@
-class Movie:
-	"""
-	A movie available for rent.
-	"""
-	# The types of movies (price_code). 
-	REGULAR = 0
-	NEW_RELEASE = 1
-	CHILDRENS = 2
-	
-	def __init__(self, title, price_code):
-		# Initialize a new movie. 
-		self.title = title
-		self.price_code = price_code
+from enum import Enum
 
-	def get_price_code(self):
-		# get the price code
-		return self.price_code
-	
-	def get_title(self):
-		return self.title
-	
-	def __str__(self):
-		return self.title
+
+class PriceCode(Enum):
+    """An enumeration for different types of movies."""
+    new_release = {
+        "price": lambda days: 3.0 * days,
+        "renter_point": lambda days: days
+    }
+    regular = {
+        "price": lambda days: 2 if days <= 2 else 2 + (1.5 * (days - 2)),
+        "renter_point": lambda days: 1
+    }
+    children = {
+        "price": lambda days: 1.5 if days <= 3 else 1.5 + (1.5 * (days - 3)),
+        "renter_point": lambda days: 1
+    }
+
+    def price(self, days: int) -> float:
+        """Return the rental price for a given number of days"""
+        pricing = self.value["price"]
+        return pricing(days)
+
+    def renter_point(self, days: int) -> int:
+        """Return the renter point for a given number of days."""
+        point = self.value["renter_point"]
+        return point(days)
+
+
+class Movie:
+    """A movie available for rent."""
+
+    def __init__(self, title, price_code: PriceCode):
+        self.title = title
+        self.price_code = price_code
+
+    def get_price_code(self):
+        return self.price_code
+
+    def get_title(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
--- rental.py
+++ rental.py
@@ -1,24 +1,34 @@
+from movie import *
+
+
 class Rental:
-	"""
-	A rental of a movie by customer.
-	From Fowler's refactoring example.
+    """
+    A rental of a movie by customer.
+    From Fowler's refactoring example.
+    A realistic Rental would have fields for the dates
+    that the movie was rented and returned, from which the
+    rental period is calculated.
+    But for simplicity of the example only a days_rented
+    field is used.
+    """
 
-	A realistic Rental would have fields for the dates
-	that the movie was rented and returned, from which the
-	rental period is calculated.
-	But for simplicity of the example only a days_rented
-	field is used.
-	"""
-	
-	def __init__(self, movie, days_rented): 
-		"""Initialize a new movie rental object for
-		   a movie with known rental period (daysRented).
-		"""
-		self.movie = movie
-		self.days_rented = days_rented
+    def __init__(self, movie: Movie, days_rented):
+        """Initialize a new movie rental object for
+           a movie with known rental period (daysRented).
+        """
+        self.movie = movie
+        self.days_rented = days_rented
 
-	def get_movie(self):
-		return self.movie
+    def get_movie(self):
+        return self.movie
 
-	def get_days_rented(self):
-		return self.days_rented
+    def get_days_rented(self):
+        return self.days_rented
+
+    def get_price(self):
+        """Compute rental change."""
+        return self.movie.get_price_code().price(self.days_rented)
+
+    def get_point(self):
+        """Award renter points."""
+        return self.movie.get_price_code().renter_point(self.days_rented)
