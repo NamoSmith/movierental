19ed526929003fd05324305fc5897323a33ad4d7
NamoSmith <namosmith2545@gmail.com>
2021-11-08 01:23:20 +0700

    Modify tests.

--- customer_test.py
+++ customer_test.py
@@ -1,41 +1,50 @@
+"""Tests for customer."""
+
+import unittest
 import re
-import unittest 
+from datetime import datetime
 from customer import Customer
 from rental import Rental
-from movie import Movie, PriceCode
+from movie import Movie
 
 
 class CustomerTest(unittest.TestCase):
-	""" Tests of the Customer class"""
-	
-	def setUp(self):
-		"""Test fixture contains:
-		
-		c = a customer
-		movies = list of some movies
-		"""
-		self.c = Customer("Movie Mogul")
-		self.new_movie = Movie("Mulan", PriceCode.new_release)
-		self.regular_movie = Movie("CitizenFour", PriceCode.regular)
-		self.children_movie = Movie("Frozen", PriceCode.childrens)
-		
-	@unittest.skip("No convenient way to test")
-	def test_billing():
-		# no convenient way to test billing since its buried in the statement() method.
-		pass
-	
-	def test_statement(self):
-		stmt = self.c.statement()
-		# visual testing
-		print(stmt)
-		# get total charges from statement using a regex
-		pattern = r".*Total [Cc]harges\s+(\d+\.\d\d).*"
-		matches = re.match(pattern, stmt, flags=re.DOTALL)
-		self.assertIsNotNone(matches)
-		self.assertEqual("0.00", matches[1])
-		# add a rental
-		self.c.add_rental(Rental(self.new_movie, 4))  # days
-		stmt = self.c.statement()
-		matches = re.match(pattern, stmt.replace('\n', ''), flags=re.DOTALL)
-		self.assertIsNotNone(matches)
-		self.assertEqual("12.00", matches[1])
+    """ Tests of the Customer class"""
+
+    def setUp(self):
+        """Test fixture contains:
+            c = a customer
+            movies = list of some movies
+        """
+
+        self.c = Customer("Movie Mogul")
+        self.new_movie = Movie("Mulan", str(datetime.now().year), ["Children"])
+        self.regular_movie = Movie("CitizenFour", "2014", [
+                                    "Documentary", "Historical Documentary"])
+        self.childrens_movie = Movie("Frozen", "2013", [
+                                    "Adventure", "Children", "Cartoon",
+                                    "Musical", "Comedy", "Fantasy"])
+
+    @unittest.skip("No convenient way to test")
+    def test_billing():
+        # no convenient way to test billing since its buried in the statement() method.
+
+        pass
+
+    def test_statement(self):
+        """Tests for statements"""
+
+        stmt = self.c.statement()
+        # visual testing
+        print(stmt)
+        # get total charges from statement using a regex
+        pattern = r".*Total [Cc]harges\s+(\d+\.\d\d).*"
+        matches = re.match(pattern, stmt, flags=re.DOTALL)
+        self.assertIsNotNone(matches)
+        self.assertEqual("0.00", matches[1])
+        # add a rental
+        self.c.add_rental(Rental(self.new_movie, 4))  # days
+        stmt = self.c.statement()
+        matches = re.match(pattern, stmt.replace('\n', ''), flags=re.DOTALL)
+        self.assertIsNotNone(matches)
+        self.assertEqual("12.00", matches[1])
--- rental_test.py
+++ rental_test.py
@@ -1,35 +1,42 @@
+"""Tests for rental."""
+
 import unittest
-from rental import Rental
-from movie import Movie, PriceCode
+from rental import Rental, PriceCode
+from movie import Movie
+from datetime import datetime
 
 
 class RentalTest(unittest.TestCase):
-	
-	def setUp(self):
-		self.new_movie = Movie("Mulan", PriceCode.new_release)
-		self.regular_movie = Movie("CitizenFour", PriceCode.regular)
-		self.children_movie = Movie("Frozen", PriceCode.childrens)
 
-	def test_movie_attributes(self):
-		"""trivial test to catch refactoring errors or change in API of Movie"""
-		m = Movie("CitizenFour", PriceCode.regular)
-		self.assertEqual("CitizenFour", m.get_title())
-		self.assertEqual(PriceCode.regular, m.get_price_code())
+    def setUp(self):
+        self.new_movie = Movie("Mulan", str(datetime.now().year), ["Children"])
+        self.regular_movie = Movie("CitizenFour", "2014", [
+            "Documentary", "Historical Documentary"])
+        self.childrens_movie = Movie("Frozen", "2013", [
+            "Adventure", "Children", "Cartoon",
+            "Musical", "Comedy", "Fantasy"])
 
-	def test_rental_price(self):
-		rental = Rental(self.new_movie, 1)
-		self.assertEqual(rental.get_price(), 3.0)
-		rental = Rental(self.new_movie, 5)
-		self.assertEqual(rental.get_price(), 15.0)
-		rental = Rental(self.regular_movie, 9)
-		self.assertEqual(rental.get_price(), 12.5)
-		rental = Rental(self.children_movie, 20)
-		self.assertEqual(rental.get_price(), 27.0)
+    def test_movie_attributes(self):
+        """trivial test to catch refactoring errors or change in API of Movie"""
+        m = Movie("CitizenFour", "2014", [
+            "Documentary", "Historical Documentary"])
+        self.assertEqual("CitizenFour", m.get_title())
+        self.assertEqual(PriceCode.regular, PriceCode.for_movie(m))
 
-	def test_rental_point(self):
-		rental = Rental(self.new_movie, 1)
-		self.assertEqual(rental.get_point(), 1)
-		rental = Rental(self.regular_movie, 6)
-		self.assertEqual(rental.get_point(), 1)
-		rental = Rental(self.children_movie, 12)
-		self.assertEqual(rental.get_point(), 1)
+    def test_rental_price(self):
+        rental = Rental(self.new_movie, 1)
+        self.assertEqual(rental.get_price(), 3.0)
+        rental = Rental(self.new_movie, 5)
+        self.assertEqual(rental.get_price(), 15.0)
+        rental = Rental(self.regular_movie, 9)
+        self.assertEqual(rental.get_price(), 12.5)
+        rental = Rental(self.childrens_movie, 20)
+        self.assertEqual(rental.get_price(), 29.0)
+
+    def test_rental_points(self):
+        rental = Rental(self.new_movie, 1)
+        self.assertEqual(rental.get_renter_point(), 1)
+        rental = Rental(self.regular_movie, 6)
+        self.assertEqual(rental.get_renter_point(), 1)
+        rental = Rental(self.childrens_movie, 12)
+        self.assertEqual(rental.get_renter_point(), 1)
