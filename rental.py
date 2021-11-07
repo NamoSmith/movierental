ecc296d22d51d3fc12d6f3ece673b1e0b053213a
NamoSmith <namosmith2545@gmail.com>
2021-11-07 22:28:27 +0700

    Move PriceCode from movie to rental.

--- rental.py
+++ rental.py
@@ -1,4 +1,31 @@
 from movie import *
+from enum import Enum
+
+
+class PriceCode(Enum):
+    """An enumeration for different types of movies."""
+    new_release = {
+        "price": lambda days: 3.0 * days,
+        "renter_point": lambda days: days
+    }
+    regular = {
+        "price": lambda days: 2 if days <= 2 else 2 + (1.5 * (days - 2)),
+        "renter_point": lambda days: 1
+    }
+    childrens = {
+        "price": lambda days: 1.5 if days <= 3 else 1.5 + (1.5 * (days - 3)),
+        "renter_point": lambda days: 1
+    }
+
+    def price(self, days: int) -> float:
+        """Return the rental price for a given number of days"""
+        pricing = self.value["price"]
+        return pricing(days)
+
+    def renter_point(self, days: int) -> int:
+        """Return the renter point for a given number of days."""
+        point = self.value["renter_point"]
+        return point(days)
 
 
 class Rental:
